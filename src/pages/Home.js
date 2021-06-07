ff6a4129085029f0597af3ad1aaaf28bd721b49a
Václav Smítka <va.smitka@gmail.com>
2021-06-07 18:10:44 +0200

    simple guide

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,12 @@ import React from "react";
 function Home() {
   return (
     <main>
+      <ol>
+        <li>Go to <a href="/settings">settings</a> prepare app for work out.</li>
+        <li>Add text simple text file with excersises. (One per line)</li>
+        <li>Submit settings.</li>
+        <li>Start workout. :)</li>
+      </ol>
     </main>
   );
 }
